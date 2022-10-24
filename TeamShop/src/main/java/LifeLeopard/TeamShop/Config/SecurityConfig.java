eb77c5cd884e08e1d41258748faf2f0f3baa26a0
Nguyen Phuoc Thinh <4601104176@student.hcmue.edu.vn>
2022-10-24 21:52:44 +0700

    update

--- TeamShop/src/main/java/LifeLeopard/TeamShop/Config/SecurityConfig.java
+++ TeamShop/src/main/java/LifeLeopard/TeamShop/Config/SecurityConfig.java
@@ -33,7 +33,8 @@ public class SecurityConfig {
     public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
         http
                 .authorizeRequests()
-                    .antMatchers("/","/admin/login","/bootstrap/**","/registration","/css/**","/fonts/**","/img/**","/js/**").permitAll()
+                    .antMatchers("/","/admin/login","/bootstrap/**","/registration","/css/**","/fonts/**","/js/**",
+                            "/images/**","/vendor/**","/product/**").permitAll()
                     .antMatchers("/admin/**").hasRole("ADMIN")
                     .antMatchers("/user/**").hasAnyRole("USER")
                     .anyRequest().authenticated()
--- TeamShop/src/main/java/LifeLeopard/TeamShop/Controllers/ProductController.java
+++ TeamShop/src/main/java/LifeLeopard/TeamShop/Controllers/ProductController.java
@@ -1,4 +1,14 @@
 package LifeLeopard.TeamShop.Controllers;
 
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping(path = "/product")
 public class ProductController {
+    @GetMapping("")
+    public String getAllProduct(){
+        return "home/product";
+    }
 }
