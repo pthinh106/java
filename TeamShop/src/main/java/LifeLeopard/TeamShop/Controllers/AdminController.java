83646fbd6eefb9fdb07375d005a6752541b73430
Nguyen Phuoc Thinh <4601104176@student.hcmue.edu.vn>
2022-11-01 15:23:29 +0700

    update code

--- TeamShop/src/main/java/LifeLeopard/TeamShop/Controllers/AdminController.java
+++ TeamShop/src/main/java/LifeLeopard/TeamShop/Controllers/AdminController.java
@@ -10,8 +10,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.*;
 
 import java.security.Principal;
 
@@ -38,8 +37,8 @@ public class AdminController {
         }
         return "admin/admin.index";
     }
-    @GetMapping("/add")
-    public String add(Model model, Principal principal){
+    @GetMapping("/product/")
+    public String getAllProduct(Model model, Principal principal){
 
         if(principal != null){
             String username = principal.getName().trim();
@@ -48,4 +47,44 @@ public class AdminController {
         }
         return "admin/admin.index";
     }
+    @GetMapping("/product/create")
+    public String createNewProduct(Model model, Principal principal){
+
+        if(principal != null){
+            String username = principal.getName().trim();
+            Employee employee =employeeRepos.findByAccountId(accountReps.findByUsername(username).getAccountId());
+            model.addAttribute("employee",employee);
+        }
+        return "admin/create-product";
+    }
+    @PostMapping("/product/create")
+    public String createProduct(Model model, Principal principal){
+        System.out.println("success");
+        if(principal != null){
+            String username = principal.getName().trim();
+            Employee employee =employeeRepos.findByAccountId(accountReps.findByUsername(username).getAccountId());
+            model.addAttribute("employee",employee);
+        }
+        return "redirect:/admin/product/create";
+    }
+    @PostMapping("/product/update/{id}")
+    public String updateProduct(Model model, Principal principal ,@PathVariable int id){
+        System.out.println(id);
+        if(principal != null){
+            String username = principal.getName().trim();
+            Employee employee =employeeRepos.findByAccountId(accountReps.findByUsername(username).getAccountId());
+            model.addAttribute("employee",employee);
+        }
+        return "redirect:/admin/product/update/" + id;
+    }
+    @GetMapping("/product/update/{id}")
+    public String getProductUpdateById(Model model, Principal principal ,@PathVariable int id){
+        if(principal != null){
+            String username = principal.getName().trim();
+            Employee employee =employeeRepos.findByAccountId(accountReps.findByUsername(username).getAccountId());
+            model.addAttribute("employee",employee);
+        }
+        return "admin/create-product";
+    }
+
 }
