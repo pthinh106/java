c90b12ce08587c4c75b05f628c5453ac71cd3d8a
Nguyen Phuoc Thinh <4601104176@student.hcmue.edu.vn>
2022-11-25 03:55:49 +0700

    update

--- TeamShop/src/main/resources/static/js/main.js
+++ TeamShop/src/main/resources/static/js/main.js
@@ -214,10 +214,14 @@
 
     /*==================================================================
     [ +/- num product ]*/
-    $('.btn-num-product-down').on('click', function(){
-        var numProduct = Number($(this).next().val());
-        if(numProduct > 0) $(this).next().val(numProduct - 1);
-    });
+    // $('.btn-num-product-down').on('click', function(){
+    //     var numProduct = Number($(this).next().val());
+    //     if(numProduct > 0) {
+    //         $(this).next().val(numProduct - 1);
+    //     }else{
+    //
+    //     }
+    // });
 
     $('.btn-num-product-up').on('click', function(){
         var numProduct = Number($(this).prev().val());
