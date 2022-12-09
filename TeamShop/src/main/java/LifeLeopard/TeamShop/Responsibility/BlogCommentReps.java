ffd404f8caa85218f1c3c8b6523cb1f8b535b04b
Phuoc Thinh <91273071+pthinh106@users.noreply.github.com>
2022-12-09 20:04:14 +0700

    Delete BlogCommentReps.java

--- TeamShop/src/main/java/LifeLeopard/TeamShop/Responsibility/BlogCommentReps.java
+++
@@ -1,7 +0,0 @@
-package LifeLeopard.TeamShop.Responsibility;
-
-import LifeLeopard.TeamShop.Models.BlogComment;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface BlogCommentReps extends JpaRepository<BlogComment,Integer> {
-}
